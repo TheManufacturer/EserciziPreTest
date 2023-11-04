06db779530465b944d8be899543186c9b6fd2560
TheManufacturer <antonio.mario.demarco@gmail.com>
2023-11-04 18:09:36 +0100

    Esercizio con i friends OK

--- src/Main.java
+++ src/Main.java
@@ -1,90 +1,66 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
-        System.out.println("Dammi la stringa da analizzare");
-        String ingr = scanner.nextLine();
+        System.out.println("Inserisci grandezza Array... ");
 
-        System.out.println("Dammi l'indice da utilizzare");
-        int ind = scanner.nextInt();
+        int grandezza = scan.nextInt();
 
-        unicode(ingr, ind);
+        scan.close();
 
-        scanner.close();
+        int[] arr = new int[grandezza];
+
+        popolArray(arr);
+        sommaValoriArr(arr);
+        maxMin(arr);
+
     }
 
-    public static void unicode(String ingresso, int indice) {
-        if (indice < 0 || indice >= ingresso.length()) {
-            System.out.println("Indice non valido.");
-        } else {
-            int inputUnicode = ingresso.codePointAt(indice);
-            System.out.println("Il carattere in Unicode è --> " + inputUnicode);
+    public static void popolArray(int[] arr) {
+
+        Random random = new Random();
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100);
+            System.out.println(arr[i]);
         }
+    }
+
+    public static void sommaValoriArr(int[] arr) {
+        int somma = 0;
+        for (int i = 0; i < arr.length; i++) {
+            somma += arr[i];
+        }
+        System.out.println("La somma è --> " + somma);
+
+    }
+
+    public static void maxMin(int[] arr) {
+        int min = arr[0];
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i]>max){
+
+                max=arr[i];
+
+            }else if(arr[i]< min){
+
+                min =arr[i];
+            }
+        }
+        System.out.println("il valore minimo è : " + min + " Il valore massimo è : " + max);
+
     }
 }
 
-
-
-
 /*
-import java.util.Scanner;
-
-public class Main { 
-
-    public static void main(String[] args){  
-        
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Dammi la stringa da analizzare");
-        String ingr = scanner.nextLine();
-
-        System.out.println("Dammi l'indice da utilizzare");
-        int ind = scanner.nextInt();
-
-        unicode(ingr, ind);
-
-        scanner.close();
-
-    }
-    
-    public static void unicode(String ingresso, int indice){
-    if (indice < 0 || indice >= ingresso.length()) {
-    System.out.println("Indice non valido.");
-} else {
-    int inputUnicode = ingresso.codePointAt(indice);
-    StringBuilder risultato = new StringBuilder("Il carattere in unicode alla posizione scelta è ");
-    risultato.append(inputUnicode);
-    System.out.println("Il carattere in Unicode è --> " + inputUnicode);
-} 
-        int inputUnicode = ingresso.codePointAt(indice);
-        
-        
-        }
-    }
-
-*/
-/*
-if(indice<=ingresso.length() ingresso.length()<=0 ){ 
-            System.out.println("Non Valido.");   
-
-        }else{
-            StringBuilder risultato = new StringBuilder("Il carattere in unicode alla posizone scelta è ");
-
-            risultato.append(inputUnicode);
-
-            System.out.println("il carattere in Unicode e --> " + inputUnicode);
-        
-
-/*
-Scrivi un programma Java che abbia un metodo che prenda il carattere in Unicode 
-    in un indice specifico all'interno di una stringa e lo stampi.
-
-    Il metodo dovrà avere nel costruttore una stringa ed una posizione e dovrà resituire 
-    il carattere in unicode o una stringa di errore.
-    Per la creazione della stringa di risultato dovrebbe essere utilizzato StringBuilder.
-
+ * Array di int random
+ * Somma di tutti i valori nell'array
+ * max e min dei valori
+ * 
  */
